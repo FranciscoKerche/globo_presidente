cce40c26491499733d9e51be7a079daefe27e4cb
FranciscoKerche <chicowkn@gmail.com>
2022-01-10 15:52:56 -0300

    Changed all main files and adapted imports

--- read_files.R
+++ read_files.R
@@ -1,39 +1,46 @@
 #### Usar pacotes ####
 
 
-# Importar valores
+# Baixar pacotes
 pacman::p_load(tidyverse, rio, lubridate)
 
 
-sci <- list.files("Pesquisa O Globo", pattern = "scie", full.names = T) %>%
-  map(import, setclass = "tibble") %>%
-  bind_rows() %>%
-  mutate(origin = "Scielo") %>%
-  select(autor = PT, titulo = BE,
-         titulo_2 = TI, titulo_3 = X1,
-         titulo_4 = Y1, revista = Z1,
-         idioma = SO, tipo = LA, keywords = DT,
-         keywords2 = DE, keyword_br = X5, keywords3 = Y5,
-         abstract = Z5, abstract_es = AB, abstract_br = X4,
-         abstract_fr = Y4, autor_afiliacao = Z4, autor_id = EM,
-         coautor_id = RI, bibliografia = OI, vinculo = U2, cidade = PU, 
-         disciplina = DI, area = C2)
+# Importar tabelas
 
-wos <- list.files("Pesquisa O Globo", pattern = "WoS", full.names = T) %>%
-  map(import, setclass = "tibble") %>%
-  bind_rows() %>%
+sci <- import("Pesquisa O Globo/scielo.xlsx", setclass = "tibble") %>%
+  select(author = AU, title = TI, title_2 = X1,
+         title_3 = Y1, title_4 = Z1,
+         publication_name = SO, idioma = LA, tipo = DT,
+         keywords = DE, keyword_2 = X5, keywords_br = Y5, keywords_4 = Z5,
+         abstract = AB, abstract_es = X4, abstract_br = Y4,
+         abstract_fr = Z4, author_afiliation = C1, email = EM,
+         researcher_id = RI, orcid = OI, cited_ref = CR, n_ref = NR, citation = TC, total_citation = Z9,
+         publisher = PU, publisher_city = PI, issn = SN, pub_year = PY, volume = VL, issue = IS,
+         begining_page = BP, end_page = EP, doi = DI, subject = EC, origin = C2, research_area = SC) %>%
+  mutate_at(vars(begining_page, end_page), as.numeric) %>%
+  mutate(pages = end_page - begining_page + 1) %>%
+  select(-end_page, -begining_page)
+
+
+wos <- import("Pesquisa O Globo/wos.xlsx", setclass = "tibble") %>%
   mutate(origin = "Web of Science") %>%
-  select(autor = PT, titulo = BE,
-         titulo_2 = TI, titulo_3 = X1,
-         titulo_4 = Y1, revista = Z1,
-         idioma = SO, tipo = LA, keywords = DT,
-         keywords2 = DE, keyword_br = X5, keywords3 = Y5,
-         abstract = Z5, abstract_es = AB, abstract_br = X4,
-         abstract_fr = Y4, autor_afiliacao = Z4, autor_id = EM,
-         coautor_id = RI, bibliografia = OI, vinculo = U2, origem_download = EC,
-         cidade = PU, disciplina = DI, area = C2)
+  select(autor = AF, titulo = TI, publication_name = SO, idioma = LA,
+         tipo = DT, conferencia = CT, ano_conf = CY,
+         cidade_conf = CL, instituicao = SP,
+         keywords = DE, keyword_plus = ID,
+         abstract = AB, author_afiliation = C1, email = EM,
+         researcher_id = RI, orcid = OI, funding = FU,
+         cited_ref = CR, n_ref = NR, citation = TC, total_citation = Z9,
+         publisher = PU, publisher_city = PI, issn = SN, pub_year = PY, volume = VL, issue = IS,
+         doi = DI, pages = PG, subject = WC, research_area = SC)
 
 
 all_files <- sci %>%
   bind_rows(wos)
 
+find_pres <- function(base, coluna, presidente){
+  base %>%
+    mutate(!!coluna = str_detect(tolower(titulo), presidente))
+}
+all_files %>%
+  find_pres(fhc, "fernando")
